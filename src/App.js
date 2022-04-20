f9d02f6069cf4188130f2bfe4ca7d8404345bf34
Chathura <chathura.madura11@gmail.com>
2022-04-21 02:22:51 +0530

    make routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
 import Sidebar from './components/Sidebar/Sidebar';
 import MainDash from './components/MainDash/MainDash';
 import RightSide from './components/RightSide/RightSide';
+import Orders from './components/Orders/Orders';
+import Customers from './components/Customers/customers';
+import Products from './components/Products/Products';
+
 
 
 function App() {
-  return (
-    <div className="App">
-     
-      <div class="AppGlass">
-      <Sidebar/>
-      <MainDash/>
-      <RightSide/>
-      </div>
 
+
+  return (
+
+    <div className="App">
+      <div class="AppGlass">
+        <Sidebar />
+
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<MainDash />}></Route>
+            <Route path="/orders" element={<Orders />}></Route>
+            <Route path="/customers" element={<Customers/>}> </Route>
+            <Route path="/products" element={<Products />}></Route>
+            <Route path="/analytics" element={<MainDash />}></Route>
+          </Routes>
+        </Router>
+
+        <RightSide />
+      </div>
     </div>
   );
 }
