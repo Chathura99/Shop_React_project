6351edb858055653506d107c2a473c429e8c1ace
Chathura <chathura.madura11@gmail.com>
2022-04-20 19:46:45 +0530

    make reusable data + dummy data

---
+++ src/Data/Data.js
@@ -0,0 +1,157 @@
+// Sidebar imports
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+  UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import {
+  UilUsdSquare,
+  UilMoneyWithdrawal
+} from "@iconscout/react-unicons";
+
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+// Recent Card Imports users
+import img1 from "../imgs/img1.png";
+import img2 from "../imgs/img2.png";
+import img3 from "../imgs/img3.png";
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Orders",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Customers",
+  },
+  {
+    icon: UilPackage,
+    heading: 'Products'
+  },
+  {
+    icon: UilChart,
+    heading: 'Analytics'
+  },
+];
+
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Charles Okla",
+    noti: "has ordered Cupcake.",
+    time: "25 seconds ago",
+  },
+  {
+    img: img2,
+    name: "Ramesh Ramkrishna",
+    noti: "has received Lasania Chiken Fri.",
+    time: "30 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Nicolai Tesla",
+    noti: "has ordered Cupcake, Lasania Chiken Fri.",
+    time: "2 hours ago",
+  },
+];
+
+
+// Analytics Cards Data for reusable components
+export const cardsData = [
+  // first card
+  {
+    title: "Sales",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barValue: 70,
+    value: "25,970",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Sales",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  // second card
+  {
+    title: "Revenue",
+    color: {
+      backGround: "linear-gradient(180deg, #FF919D 0%, #FC929D 100%)",
+      boxShadow: "0px 10px 20px 0px #FDC0C7",
+    },
+    barValue: 80,
+    value: "14,270",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  // third card
+  {
+    title: "Expenses",
+    color: {
+      backGround:
+        "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
+      boxShadow: "0px 10px 20px 0px #F9D59B",
+    },
+    barValue: 60,
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
+//table data
+export const tableData = [
+{
+  col1: 'Lasania Chiken Fri',
+  col2: 18908424,
+  col3: '2 March 2022',
+  col4: 'Approved',
+  col5: 'Detail'
+},
+{
+  col1: 'Big Baza Bang ',
+  col2: 18908424,
+  col3: '2 March 2022',
+  col4: 'Pending',
+  col5: 'Detail'
+},
+{
+  col1: 'Cupcake',
+  col2: 18908424,
+  col3: '2 March 2022',
+  col4: 'Delivered',
+  col5: 'Detail'
+},
+{
+  col1: 'Lasania Chiken Fri',
+  col2: 18908424,
+  col3: '2 March 2022',
+  col4: 'Approved',
+  col5: 'Detail'
+},
+];
