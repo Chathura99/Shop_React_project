73efa989f282534c9ac0195bf2f9408ab324fbdc
Chathura <chathura.madura11@gmail.com>
2022-04-21 02:23:48 +0530

    initialize order,product,customer component

---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Orders = () => {
+    return (
+        <div className="Orders">
+            <h1>Orders</h1>
+
+
+        </div>
+    );
+};
+
+export default Orders;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Products = () => {
+    return (
+        <div className="Products">
+            <h1>Products</h1>
+
+
+        </div>
+    );
+};
+
+export default Products;
